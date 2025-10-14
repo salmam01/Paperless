0ef2acaeb7b6bdc1d4c11a24189f9fd8420918f2
salmam01 <if23b281@technikum-wien.at>
2025-10-14 21:22:56 +0200

    Added DocumentPublisher & Paperless.Services

---
+++ Paperless/Paperless.API/Messaging/DocumentPublisher.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+
+namespace Paperless.API.Messaging
+{
+    public class DocumentPublisher
+    {
+        //  Add to configuration file later
+        private readonly IConnection _connection;
+        private readonly int Port = 5672;
+        private readonly string _queueName = "ocrQueue";
+        private readonly ILogger<DocumentPublisher> _logger;
+
+        public DocumentPublisher()
+        {
+            
+        }
+
+        public void PublishDocument()
+        {
+            try
+            {
+                
+            } catch {
+                /*
+                using var connection = _connection.CreateConnection();
+                using var channel = connection.CreateChannel();
+                */
+            }
+        }
+    }
+}
---
+++ Paperless/Paperless.Services/Program.cs
@@ -0,0 +1,7 @@
+using Paperless.Services;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
