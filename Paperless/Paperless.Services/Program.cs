7b97c845cfaa34cb9a4b44265ca73d4f482c0029
Zeliha <zeliha@outlook.com>
2025-10-15 18:09:51 +0200

    Configure RabbitMQ settings using RabbitMqConfig in Program.cs

--- Paperless/Paperless.Services/Program.cs
+++ Paperless/Paperless.Services/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.Extensions.Options;
 using Paperless.Services;
+using Paperless.Services.Configurations;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.Configure<RabbitMqConfig>(builder.Configuration.GetSection("RabbitMQ"));
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
