00ff7d6ec549159cf68415098b63cd65d4f2682d
Zeliha <zeliha@outlook.com>
2025-11-13 23:35:23 +0100

    Integrate DbContext, repositories, and GenAI configuration into service setup.

--- Paperless/Paperless.Services/Program.cs
+++ Paperless/Paperless.Services/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Minio;
+using Paperless.DAL.Database;
+using Paperless.DAL.Repositories;
 using Paperless.Services.Configurations;
 using Paperless.Services.Workers;
 using Paperless.Services.Services;
@@ -15,12 +18,24 @@ Log.Logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog();
 
+// Configuration
 builder.Services.Configure<RabbitMqConfig>(builder.Configuration.GetSection("RabbitMq"));
 builder.Services.Configure<MinIoConfig>(builder.Configuration.GetSection("MinIo"));
 builder.Services.Configure<OcrConfig>(builder.Configuration.GetSection("Ocr"));
+builder.Services.Configure<GenAIConfig>(builder.Configuration.GetSection("GenAI"));
 
+// Database
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<PaperlessDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Repositories
+builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
+
+// Services
 builder.Services.AddSingleton<StorageService>();
 builder.Services.AddSingleton<OcrService>();
+builder.Services.AddScoped<DocumentUpdateService>(); 
 builder.Services.AddHostedService<OcrWorker>();
 
 var host = builder.Build();
